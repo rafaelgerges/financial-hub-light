48f2edd598cae5a56c06d6378c58af6fce8d37cf
Rafael Gerges <rafael@laepi.com.br>
2026-01-31 09:56:57 -0300

    removendo a pasta dist

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-35c88359'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.sh4u72vkjno"
+    "revision": "0.372vihdifn8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
